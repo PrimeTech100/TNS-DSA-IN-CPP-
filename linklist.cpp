f7af851e53d8747410f1bb4bbdbf7b6e7f6d4ad3
Sumit Ubale <91335224+PrimeTech100@users.noreply.github.com>
2023-07-12 04:35:33 +0000

    add -A

--- linklist.cpp
+++ linklist.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+class Node
+{
+    int data;
+    Node *next;
+};
+class Linklist 
+{
+    int data;
+    Node *next;
+    public:
+        Node(int no)
+        {
+            data = no;
+            next = NULL;
+        }
+};
+class linklist
+{
+    Node *head;
+    public:Linklist(
+        {
+            head = NULL;
+        }
+        void create()
+        {
+            int no;
+            std::cout<<"Enter the no. up to 999..";
+            std::cin>>no;
+            while(no != 999)
+            {
+                Node *newnode = new Node(no);
+                if(head == NULL)
+                {
+                    head = newnode;
+                }
+                else
+                {
+                    Node *temp = head;
+                    while(temp->next != NULL)
+                    {
+                        temp = temp->next;
+                    }
+                    temp->next = newnode;
+                }
+                std::cout<<"Enter the no. up to 999..";
+                std::cin>>no;
+            }
+        }
 
-int main (){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
-}
+    )
+};
